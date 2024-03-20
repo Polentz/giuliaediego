b11651a1304f2cc4403e31ba8cdab6ed955aaaa5
Giulia Polenta <giuliapolenta@gmail.com>
2024-03-20 13:08:13 +0100

    Try responsive for big screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,6 @@ const handleInternalLinks = () => {
     anchorTags.forEach(link => {
         link.addEventListener("click", (event) => {
             event.preventDefault();
-            [...anchorTags].filter(i => i !== link).forEach(i => i.classList.remove("--current"));
-            link.classList.toggle("--current");
             const href = link.getAttribute("href");
             document.querySelector(href).scrollIntoView({
                 behavior: "smooth"
@@ -18,29 +16,29 @@ const handleInternalLinks = () => {
     });
 };
 
-const handleScrolledElements = () => {
-    const header = document.querySelector(".header-text");
-    gsap.set(header, {
-        scale: 1.4,
-        duration: 0.5,
-    });
-    window.addEventListener("scroll", () => {
-        let lastScrollTop = 40;
-        let scrollTop = window.scrollY || document.documentElement.scrollTop;
-        if (scrollTop < lastScrollTop) {
-            gsap.to(header, {
-                scale: 1.4,
-                duration: 0.25,
-            });
-        } else {
-            gsap.to(header, {
-                scale: 1,
-                duration: 0.25,
-            });
-        };
-        lastScrollTop = scrollTop;
-    });
-};
+// const handleScrolledElements = () => {
+//     const header = document.querySelector(".header-text");
+//     gsap.set(header, {
+//         scale: 1.4,
+//         duration: 0.5,
+//     });
+//     window.addEventListener("scroll", () => {
+//         let lastScrollTop = 40;
+//         let scrollTop = window.scrollY || document.documentElement.scrollTop;
+//         if (scrollTop < lastScrollTop) {
+//             gsap.to(header, {
+//                 scale: 1.4,
+//                 duration: 0.25,
+//             });
+//         } else {
+//             gsap.to(header, {
+//                 scale: 1,
+//                 duration: 0.25,
+//             });
+//         };
+//         lastScrollTop = scrollTop;
+//     });
+// };
 
 const observeFooter = () => {
     const targets = document.querySelectorAll(".footer-item");
@@ -64,13 +62,48 @@ const observeFooter = () => {
     targets.forEach(target => {
         observer.observe(target);
     });
-}
+};
+
+const observeSections = () => {
+    if (window.scrollY || document.documentElement.scrollTop) {
+        document.getElementById("benvenuti").classList.add("--current");
+    };
+    const sections = document.querySelectorAll(".section, .footer");
+    const navItems = document.querySelectorAll(".nav-item.js-href");
+    const options = {
+        threshold: 0.25,
+    };
+    const handleIntersection = (entries) => {
+        entries.map((entry) => {
+            if (entry.isIntersecting) {
+                navItems.forEach(item => {
+                    const itemId = item.dataset.id;
+                    if (itemId === entry.target.id) {
+                        [...navItems].filter(i => i !== item).forEach(i => i.classList.remove("--current"));
+                        item.classList.add("--current");
+                    };
+                });
+            } else if (window.scrollY || document.documentElement.scrollTop) {
+                document.getElementById("benvenuti").classList.add("--current");
+            };
+        });
+    };
+    const observer = new IntersectionObserver(handleIntersection, options);
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+};
+
 
 window.addEventListener("load", () => {
     documentHeight();
     handleInternalLinks();
     observeFooter();
 });
+
+window.addEventListener("scroll", () => {
+    observeSections();
+})
 
 window.addEventListener("resize", () => {
     documentHeight();
