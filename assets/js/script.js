ba6c77cbd1f02bd8d755661d88ffc033d3dfb99f
Giulia Polenta <giuliapolenta@gmail.com>
2024-03-20 10:49:09 +0100

    Finish content

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,72 @@ const documentHeight = () => {
 };
 
 const handleInternalLinks = () => {
-    const anchorTags = document.querySelectorAll(".nav-item");
+    const anchorTags = document.querySelectorAll(".js-href");
     anchorTags.forEach(link => {
         link.addEventListener("click", (event) => {
             event.preventDefault();
+            [...anchorTags].filter(i => i !== link).forEach(i => i.classList.remove("--current"));
+            link.classList.toggle("--current");
             const href = link.getAttribute("href");
-            // const offset = document.querySelector(".header").offsetHeight;
-            const elementPosition = document.querySelector(href).getBoundingClientRect().top;
-            // const offsetPosition = elementPosition + window.scrollY - offset;
-            window.scrollTo({
-                top: elementPosition,
+            document.querySelector(href).scrollIntoView({
                 behavior: "smooth"
             });
         });
     });
 };
 
-// const marquee = () => {
-//     const marquee = document.querySelectorAll(".marquee-part");
-//     marquee.forEach(el => {
-//         let rate = 70;
-//         let distance = el.clientWidth;
-//         let style = window.getComputedStyle(el);
-//         let marginRight = parseInt(style.marginRight) || 0;
-//         let totalDistance = distance + marginRight;
-//         let time = totalDistance / rate;
-//         let container = el.parentElement;
-//         gsap.to(container, time, {
-//             repeat: -1,
-//             x: '-' + totalDistance,
-//             ease: Linear.easeNone,
-//         });
-//     });
-// };
+const handleScrolledElements = () => {
+    const header = document.querySelector(".header-text");
+    gsap.set(header, {
+        scale: 1.4,
+        duration: 0.5,
+    });
+    window.addEventListener("scroll", () => {
+        let lastScrollTop = 40;
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        if (scrollTop < lastScrollTop) {
+            gsap.to(header, {
+                scale: 1.4,
+                duration: 0.25,
+            });
+        } else {
+            gsap.to(header, {
+                scale: 1,
+                duration: 0.25,
+            });
+        };
+        lastScrollTop = scrollTop;
+    });
+};
+
+const observeFooter = () => {
+    const targets = document.querySelectorAll(".footer-item");
+    const marquee = document.querySelector(".marquee");
+    const headline = document.querySelector("#regalo .text-headline");
+    const options = {
+        threshold: 0.5,
+    };
+    const handleIntersection = (entries) => {
+        entries.map((entry) => {
+            if (entry.isIntersecting) {
+                marquee.style.opacity = "0";
+                headline.style.opacity = "0";
+            } else {
+                marquee.style.opacity = "1";
+                headline.style.opacity = "1";
+            };
+        });
+    };
+    const observer = new IntersectionObserver(handleIntersection, options);
+    targets.forEach(target => {
+        observer.observe(target);
+    });
+}
 
 window.addEventListener("load", () => {
     documentHeight();
-    // handleInternalLinks();
+    handleInternalLinks();
+    observeFooter();
 });
 
 window.addEventListener("resize", () => {
